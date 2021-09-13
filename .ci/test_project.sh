136e75257c330bb3ec1f49173aa989203dde1bc4
DerThorsten <derthorstenbeier@gmail.com>
2021-09-13 16:01:02 +0200

    updated ci

--- .ci/test_project.sh
+++ .ci/test_project.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# echo "------------------------------------------------------------"
-# echo "create cookiecutter-dev-requirements conda env"
-# echo "------------------------------------------------------------"
-# conda env create -f cookiecutter-dev-requirements.yml || exit 1
+echo "------------------------------------------------------------"
+echo "create cookiecutter-dev-requirements conda env"
+echo "------------------------------------------------------------"
+conda env create -f cookiecutter-dev-requirements.yml || exit 1
 
-# echo "------------------------------------------------------------"
-# echo "activate cookiecutter-dev-requirements conda env"
-# echo "------------------------------------------------------------"
-# source activate cookiecutter-dev-requirements || exit 1
+echo "------------------------------------------------------------"
+echo "activate cookiecutter-dev-requirements conda env"
+echo "------------------------------------------------------------"
+source activate cookiecutter-dev-requirements || exit 1
 
 echo "------------------------------------------------------------"
 echo "run bash test"
