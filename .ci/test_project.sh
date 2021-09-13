2be422dc954431cf978bf82d815cbcf3817455cc
DerThorsten <derthorstenbeier@gmail.com>
2021-09-13 15:54:12 +0200

    updated ci

--- .ci/test_project.sh
+++ .ci/test_project.sh
@@ -1,44 +1,18 @@
 #!/bin/bash
 
 echo "------------------------------------------------------------"
-echo "create xeus_cookiecutter-dev-requirements conda env"
+echo "create cookiecutter-dev-requirements conda env"
 echo "------------------------------------------------------------"
-conda env create -f xeus_cookiecutter-dev-requirements.yml || exit 1
+conda env create -f cookiecutter-dev-requirements.yml || exit 1
 
 echo "------------------------------------------------------------"
-echo "activate xeus_cookiecutter-dev-requirements conda env"
+echo "activate cookiecutter-dev-requirements conda env"
 echo "------------------------------------------------------------"
-source activate xeus_cookiecutter-dev-requirements || exit 1
+source activate cookiecutter-dev-requirements || exit 1
 
 echo "------------------------------------------------------------"
-echo "run pytest"
+echo "run bash test"
 echo "------------------------------------------------------------"
-make test || exit 1
-
-echo "------------------------------------------------------------"
-echo "run coverage"
-echo "------------------------------------------------------------"
-make coverage-no-html || exit 1
-
-
-echo "------------------------------------------------------------"
-echo "make dist"
-echo "------------------------------------------------------------"
-make dist || exit 1
-
-echo "------------------------------------------------------------"
-echo "make clean"
-echo "------------------------------------------------------------"
-make clean || exit 1
-
-echo "------------------------------------------------------------"
-echo "make help"
-echo "------------------------------------------------------------"
-make help || exit 1
-
-
-echo "------------------------------------------------------------"
-echo "install via setup.py"
-echo "------------------------------------------------------------"
-python setup.py install || exit 1
+cd tests
+./bash_test || exit 1
 
