bb7e24736fbdceb12c9742e9346860d737f57a61
DerThorsten <derthorstenbeier@gmail.com>
2021-09-15 14:47:19 +0200

    more fixes

--- tests/test_version.py
+++ tests/test_version.py
@@ -12,18 +12,3 @@ class TestBake(object):
         assert result.project_path.is_dir()
 
 
-
-        # remember the directory where tests should be run from
-        cwd = os.getcwd()
-        # change directories to the generated project directory 
-        os.chdir(str(project.project))
-        try:
-            pass
-        except sh.ErrorReturnCode as e:
-            # print the error, so we know what went wrong
-            print(e)
-            # make sure the test fails
-            pytest.fail(e)
-        finally:
-            # always change directories to the test directory
-            os.chdir(cwd)
--- {{cookiecutter.github_project_name}}/src/xinterpreter.cpp
+++ {{cookiecutter.github_project_name}}/src/xinterpreter.cpp
@@ -137,8 +137,10 @@ namespace {{cookiecutter.cpp_namespace}}
         nl::json result;
         result["status"] = "ok";
         result["found"] = true;
+        {% raw %}
         result["data"] = {{std::string("text/plain"), std::string("hello!")}};
         result["metadata"] = {{std::string("text/plain"), std::string("hello!")}};
+        {% endraw %} 
         return result;
     }
 
