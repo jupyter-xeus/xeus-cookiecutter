5236b84c4a035d523be47900e37ec0665bafda83
DerThorsten <derthorstenbeier@gmail.com>
2021-09-15 13:21:14 +0200

    fixed code

--- {{cookiecutter.github_project_name}}/src/xinterpreter.cpp
+++ {{cookiecutter.github_project_name}}/src/xinterpreter.cpp
@@ -30,7 +30,7 @@ namespace {{cookiecutter.cpp_namespace}}
     }
 
     nl::json interpreter::execute_request_impl(int execution_counter, // Typically the cell number
-                                                      const std::string& /*code*/, // Code to execute
+                                                      const std::string& code, // Code to execute
                                                       bool /*silent*/,
                                                       bool /*store_history*/,
                                                       nl::json /*user_expressions*/,
