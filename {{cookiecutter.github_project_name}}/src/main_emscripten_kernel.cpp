2d8e792b6d0a3efd10b13324865ce50dcdeefd14
DerThorsten <derthorstenbeier@gmail.com>
2021-10-23 18:37:35 +0200

    emscripten build

---
+++ {{cookiecutter.github_project_name}}/src/main_emscripten_kernel.cpp
@@ -0,0 +1,24 @@
+/***************************************************************************
+* Copyright (c) {% now 'utc', '%Y' %},                                          
+*                                                                          
+* Distributed under the terms of the {{cookiecutter.open_source_license}}.                 
+*                                                                          
+* The full license is in the file LICENSE, distributed with this software. 
+****************************************************************************/
+
+#include <iostream>
+#include <memory>
+
+
+#include <emscripten/bind.h>
+
+#include "{{cookiecutter.cpp_root_folder}}/xinterpreter.hpp"
+#include "xeus/xembind.hpp"
+
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    xeus::export_core();
+    using interpreter_type = {{cookiecutter.cpp_namespace}}::interpreter;
+    xeus::export_kernel<interpreter_type>("xkernel");
+}
+
