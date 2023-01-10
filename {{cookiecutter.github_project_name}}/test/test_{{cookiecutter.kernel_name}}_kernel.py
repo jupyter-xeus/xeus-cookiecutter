4d6386d208a970600c16e12dae926c1a6b7a76cb
Hind Montassif <hind.montassif@gmail.com>
2023-01-10 17:57:19 +0100

    Comment irrelevant tests

--- {{cookiecutter.github_project_name}}/test/test_{{cookiecutter.kernel_name}}_kernel.py
+++ {{cookiecutter.github_project_name}}/test/test_{{cookiecutter.kernel_name}}_kernel.py
@@ -23,18 +23,26 @@ class KernelTests(jupyter_kernel_test.KernelTests):
     invalid_code_samples = ["invalid"]
     code_inspect_sample = "print"
 
-    def test_stdout(self):
-        self.flush_channels()
-        reply, output_msgs = self.execute_helper(code="hello, world")
-        self.assertEqual(output_msgs[0]["msg_type"], "stream")
-        self.assertEqual(output_msgs[0]["content"]["name"], "stdout")
-        self.assertEqual(output_msgs[0]["content"]["text"], "hello, world")
+    #######################################################################################
+    # TODO Uncomment these code lines in order to enable the tests (to be modified/adapted)
+    #######################################################################################
+    # These tests examples check that the given code "hello, world" would be published as
+    # text stdout stream message and "error" as stderr
+    # Other tests are performed inside the jupyter_kernel_test `execute_helper` function
+    # and based on the variables defined above
 
-    def test_stderr(self):
-        self.flush_channels()
-        reply, output_msgs = self.execute_helper(code="error")
-        self.assertEqual(output_msgs[0]["msg_type"], "stream")
-        self.assertEqual(output_msgs[0]["content"]["name"], "stderr")
+    #def test_stdout(self):
+        #self.flush_channels()
+        #reply, output_msgs = self.execute_helper(code="hello, world")
+        #self.assertEqual(output_msgs[0]["msg_type"], "stream")
+        #self.assertEqual(output_msgs[0]["content"]["name"], "stdout")
+        #self.assertEqual(output_msgs[0]["content"]["text"], "hello, world")
+
+    #def test_stderr(self):
+        #self.flush_channels()
+        #reply, output_msgs = self.execute_helper(code="error")
+        #self.assertEqual(output_msgs[0]["msg_type"], "stream")
+        #self.assertEqual(output_msgs[0]["content"]["name"], "stderr")
 
 
 if __name__ == "__main__":
